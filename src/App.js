70a9671255bd93ca277054cbab129b32e6132de4
youngeun choi <choi46910@naver.com>
2022-09-01 21:27:47 +0900

    firebase 연결

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
@@ -6,6 +7,15 @@ import Products from './components/Shop/Products';
 function App() { //useSelector 부분 이해 아직 못했음 😅 값 추출부분
   const showCart = useSelector((state) => state.ui.cartIsVisible)
   // 리덕스 상태를 자동으로 받는 함수를 전달해야한다.
+  const cart = useSelector(state => state.cart);
+
+  useEffect(() => {
+    fetch('https://react-data-a6373-default-rtdde.firebaseio.com/cart.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart)
+    });
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
