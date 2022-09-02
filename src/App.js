319a7eecce674b1b24831d259c9ec73db20afe86
youngeun choi <choi46910@naver.com>
2022-09-02 21:43:56 +0900

    데이터 알림 추가

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { Fragment, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { uiActions } from './store/ui-slice';
+import Notification from './components/UI/Notification';
+
+let isInitial = true;
 
 function App() { //useSelector 부분 이해 아직 못했음 😅 값 추출부분
-  const showCart = useSelector((state) => state.ui.cartIsVisible)
+  const dispatch = useDispatch();
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
   // 리덕스 상태를 자동으로 받는 함수를 전달해야한다.
   const cart = useSelector(state => state.cart);
+  const notification = useSelector(state => state.ui.notification);
 
   useEffect(() => {
-    fetch('https://react-data-a6373-default-rtdde.firebaseio.com/cart.json', {
-      method: 'PUT',
-      body: JSON.stringify(cart)
+    const sendCartData = async () => {
+      dispatch(uiActions.showNotification({
+        status: 'pending',
+        title: 'Sending..',
+        message: 'Sending cart data!',
+      }))
+      const response = await fetch('https://react-data-a6373-default-rtdde.firebaseio.com/cart.json', {
+        method: 'PUT',
+        body: JSON.stringify(cart)
+      });
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.');
+      }
+
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title: 'Success!',
+        message: 'Sending cart data successfully!',
+      }))
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title: 'Error!',
+        message: 'Sending cart data failed!',
+      }))
     });
-  }, [cart]);
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message} />)}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -2,10 +2,17 @@ import { createSlice } from '@reduxjs/toolkit'; //1. createSlice 불러오기
 
 const uiSlice = createSlice({ //2.가져오기 
   name: 'ul',
-  initialState: { cartIsVisible: false }, //장바구니 표시혀부 제어
+  initialState: { cartIsVisible: false, notification: null }, //장바구니 표시혀부 제어
   reducers: { //3. 리듀서 키 작성, 작업을 나타내는 메소드 정리
     toggle(state) {
       state.cartIsVisible = !state.cartIsVisible; //이전상태 반대로 설정
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
     }
   }
 });
